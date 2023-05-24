4aec697d0549a4564ecc40dafe3daf9e9c3b5f08
MATHEUS WAGATUMA SPADRIZANI <spadrizani.8935@aluno.pr.senac.br>
2023-05-24 12:13:21 -0300

    sazonales

--- sazonal.php
+++ sazonal.php
@@ -9,6 +9,8 @@
 </head>
 <body>
     
+<h1>COMEÇOU O OUTONO !</h1>
+
 <div class="card text-bg-success mb-3" style="max-width: 900px;">
   <div class="row g-0">
     <div class="col-md-4">
@@ -26,13 +28,95 @@
             <br>
             Espresso Cinnamon Toffee
         </p>
-        <button type="button" class="btn btn-secondary">c</button>
+        <button type="button" class="btn btn-secondary">Compre já</button>
         <p class="card-text"><small class="text-body-secondary">Disponível de 20/03/2023 a 21/06/2023</small></p>
       </div>
     </div>
   </div>
 </div>
 </div>
+
+
+<div class="card text-bg-success mb-3" style="max-width: 900px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="IMG/outono2.png" class="img-fluid rounded-start">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h3 class="card-title">Linha de Outono</h3>
+        <p class="card-text">
+          <br>
+            Mocha Cookie Frappuccino
+            <br>
+            Espresso Mocha Cookie
+        </p>
+        <button type="button" class="btn btn-secondary">Compre já</button>
+        <p class="card-text"><small class="text-body-secondary">Disponível de 20/03/2023 a 21/06/2023</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+<br>
+
+<h1>VIVA A ABÓBORA</h1>
+
+<div class="card text-bg-success mb-3" style="max-width: 900px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="IMG/aboboras.jpg" class="img-fluid rounded-start">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h3 class="card-title">Linha de Abóboras</h3>
+        <p class="card-text">
+          
+            Pumpkin Spice Frappuccino
+            <br>
+            Iced Pumpkin Spice Latte
+            <br>
+            Pumpkin Spice Latte
+            <br>
+            Pumpkin Spice Doubleshot Ice Shaken
+            <br>
+            Sweet Pumpkin Blondie
+        </p>
+        <button type="button" class="btn btn-secondary">Compre já</button>
+        <p class="card-text"><small class="text-body-secondary">Disponível de 21/08/2023 a 21/09/2023</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+<div class="card text-bg-success mb-3" style="max-width: 900px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="IMG/aboboras2.jpg" class="img-fluid rounded-start">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h3 class="card-title">Linha de Abóboras</h3>
+        <p class="card-text">
+          
+            Pumpkin Spice Latte Frappuccino
+            <br>
+            Pumpkin Spice Latte
+            <br>
+            Iced Pumpkin Spice Latte
+            <br>
+            Pumpkin Spice Latte Cold Brew Cold Foam
+        </p>
+        <button type="button" class="btn btn-secondary">Compre já</button>
+        <p class="card-text"><small class="text-body-secondary">Disponível de 21/08/2023 a 21/09/2023</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+<br>
+
 
 
 </body>
