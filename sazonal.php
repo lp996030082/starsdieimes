ad7803d3080aba1fb1064deb0f761481dcb0eead
MATHEUS WAGATUMA SPADRIZANI <spadrizani.8935@aluno.pr.senac.br>
2023-05-24 08:08:46 -0300

    sazonal

---
+++ sazonal.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Star Nunes</title>
+</head>
+<body>
+    
+<div class="card text-bg-success mb-3" style="max-width: 900px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="IMG/outono.png" class="img-fluid rounded-start">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h3 class="card-title">Linha de Outono</h3>
+        <p class="card-text">
+            Cinnamon Toffee Frappuccino
+            <br>
+            Iced Cinnamon Toffee Latte
+            <br>
+            Cinnamon Toffee Latte
+            <br>
+            Espresso Cinnamon Toffee
+        </p>
+        <button type="button" class="btn btn-secondary">c</button>
+        <p class="card-text"><small class="text-body-secondary">Disponível de 20/03/2023 a 21/06/2023</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</html>
