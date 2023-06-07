b6a0dab72a04911fb02ff49feeb009f451935d5d
LETICIA ARAUJO PEREIRA <leticia.1970@aluno.pr.senac.br>
2023-06-07 07:36:04 -0300

    salvar

---
+++ bebidas_tradicionais.php
@@ -0,0 +1,272 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Star Nunes</title>
+</head>
+<body>
+<div class=cardápio>
+ <p> Cardápio bebidas tradicionais </p>
+</div>
+    
+<h1>Expressos</h1>
+
+<div class="card text-bg-success mb-3" style="max-width: 900px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="imagens\café2.jpg" class="img-fluid rounded-start">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h3 class="card-title">Expressos</h3>
+        <p class="card-text">
+            Café filtrado
+            <br>
+            Expresso com chantilly
+            <br>
+            Expresso tradicional ou machiatto
+        </p>
+        <button type="button" class="btn btn-secondary">Compre já</button>
+        <p class="card-text"><small class="text-body-secondary"></small></p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+
+<div class="card text-bg-success mb-3" style="max-width: 900px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="imagens\espresso-starbucks.jpg" class="img-fluid rounded-start">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h3 class="card-title">Expresso Collection</h3>
+        <p class="card-text">
+          <br>
+            Avelã
+            <br>
+           Brigadeiro
+        </p>
+        <button type="button" class="btn btn-secondary">Compre já</button>
+        <p class="card-text"><small class="text-body-secondary"></small></p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+<br>
+
+<h1>Cafés</h1>
+
+<div class="card text-bg-success mb-3" style="max-width: 900px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="imagens\cafés7.jpg" class="img-fluid rounded-start">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h3 class="card-title">Cafés</h3>
+        <p class="card-text">
+          
+            Café Americano
+            <br>
+            Café com leite
+            <br>
+            Prensa Francesa
+        </p>
+        <button type="button" class="btn btn-secondary">Compre já</button>
+        <p class="card-text"><small class="text-body-secondary"></small></p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+<div class="card text-bg-success mb-3" style="max-width: 900px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="imagens\expresso4.jpg" class="img-fluid rounded-start">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h3 class="card-title">Clássicos</h3>
+        <p class="card-text">
+          
+            Café latte
+            <br>
+            Flat white
+            <br>
+           Café mocha ou café mocha branco
+            <br>
+            Doce de leite latte
+            <br>
+            Caramelo macchiato
+            <br>
+            Cappuccino tradicional
+            <br>
+            Cappuccino canela
+            <br>
+            Cappuccino chocolate
+        </p>
+        <button type="button" class="btn btn-secondary">Compre já</button>
+        <p class="card-text"><small class="text-body-secondary"></small></p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+<br>
+
+<h1>Chocolates</h1>
+
+<div class="card text-bg-success mb-3" style="max-width: 900px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="imagens\chocolates_starbucks.jpg" class="img-fluid rounded-start">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h3 class="card-title">Cafés</h3>
+        <p class="card-text">
+          
+           Chocolate clássico
+            <br>
+            Chocolate branco
+        </p>
+        <button type="button" class="btn btn-secondary">Compre já</button>
+        <p class="card-text"><small class="text-body-secondary"></small></p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+<br>
+
+<h1>Cold brew</h1>
+
+<div class="card text-bg-success mb-3" style="max-width: 900px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="imagens\coldbrew.jpg" class="img-fluid rounded-start">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h3 class="card-title">Cold brew</h3>
+        <p class="card-text">
+          
+           Nitro
+            <br>
+            Tradicional
+            <br>
+            Com leite    
+            <br>
+            Baunilha
+        </p>
+        <button type="button" class="btn btn-secondary">Compre já</button>
+        <p class="card-text"><small class="text-body-secondary"></small></p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+<br>
+
+<h1>Frappuccino</h1>
+
+<div class="card text-bg-success mb-3" style="max-width: 900px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="imagens\frappuccino.jpg" class="img-fluid rounded-start">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h3 class="card-title">Frappuccinos - Base leite</h3>
+        <p class="card-text">
+          
+            Chocolate ou chocolate branco
+            <br>
+            Super morango
+            <br>
+            Brigadeiro    
+            <br>
+            Doce de leite
+            <br>
+            Baunilha
+        </p>
+        <button type="button" class="btn btn-secondary">Compre já</button>
+        <p class="card-text"><small class="text-body-secondary"></small></p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+<br>
+
+<h1>Frappuccino</h1>
+
+<div class="card text-bg-success mb-3" style="max-width: 900px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="imagens\frappuccino2.jpg" class="img-fluid rounded-start">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h3 class="card-title">Frappuccinos - Base café</h3>
+        <p class="card-text">
+          
+           Café
+            <br>
+            Caramelo
+            <br>
+            Brigadeiro 
+            <br>
+            Doce de leite
+            <br>
+            Mocha ou mocha branco
+            <br>
+            Choco chip
+        </p>
+        <button type="button" class="btn btn-secondary">Compre já</button>
+        <p class="card-text"><small class="text-body-secondary"></small></p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+<br>
+
+<h1>Chás</h1>
+
+<div class="card text-bg-success mb-3" style="max-width: 900px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="imagens\chástarbucks.jpg" class="img-fluid rounded-start">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h3 class="card-title">Chás - Gelado ou quente</h3>
+        <p class="card-text">
+          
+           Chá gelado com limonada e blueberry
+            <br>
+            Chá gelado com limonada 
+            <br>
+           Chá latte quente ou gelado  
+        </p>
+        <button type="button" class="btn btn-secondary">Compre já</button>
+        <p class="card-text"><small class="text-body-secondary"></small></p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</html>
